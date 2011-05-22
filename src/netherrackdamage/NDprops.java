4a4144a841995c945c50be1e804ab221f607a47c
Christopher Cox <timesplitter08@googlemail.com>
2011-05-22 11:15:30 +0100

    Cleanup commit.

---
+++ src/netherrackdamage/NDprops.java
@@ -0,0 +1,58 @@
+package netherrackdamage;
+ 
+import java.io.*;
+import java.util.*;
+import org.bukkit.util.config.Configuration;
+ 
+public class NDprops {
+    HashMap<String,ArrayList<String>> Properties = new HashMap<String,ArrayList<String>>(); 
+    String fileName;
+    Configuration file;
+    private final NetherrackDamage plugin;
+    int damageDealt;
+    int damageDelay;
+    String blockID;
+    String bootMod;
+    String temp;
+
+    public NDprops(NetherrackDamage instance) {
+        plugin = instance;
+        
+    }
+    public void doConfig() {
+        file = new Configuration(new File(plugin.getDataFolder(), "properties.yml"));
+        file.load();
+        if (new File(plugin.getDataFolder(),"properties.yml").exists()) {
+            System.out.println("[Netherrack-Damage] Configuration file loaded!");
+        } else {
+            file.setProperty("damageDealt", 1);
+            file.setProperty("damageDelay", 1);
+            file.setProperty("protectedWorlds", "No");
+            file.setProperty("blockID", "87");
+            file.setProperty("bootMod", "Yes");
+            file.save();
+            System.out.println("[Netherrack-Damage] Configuration file created with default values!");
+        }
+        
+        //Get configs
+        damageDealt = file.getInt("damageDealt", damageDealt);
+        damageDelay = file.getInt("damageDelay", damageDelay);
+        bootMod = file.getString("bootMod", bootMod);
+        blockID = file.getString("blockID", blockID);
+        temp = file.getString("protectedWorlds", temp);
+        
+    }
+    public void relConfig() {
+        file = new Configuration(new File(plugin.getDataFolder(), "properties.yml"));
+        file.load();
+        if (new File(plugin.getDataFolder(),"properties.yml").exists()) {
+            System.out.println("[Netherrack-Damage] Configuration file reloaded!");
+        }
+        //Get configs
+        damageDealt = file.getInt("damageDealt", damageDealt);
+        damageDelay = file.getInt("damageDelay", damageDelay);
+        bootMod = file.getString("bootMod", bootMod);
+        blockID = file.getString("blockID", blockID);
+        temp = file.getString("protectedWorlds", temp);
+    }
+}
